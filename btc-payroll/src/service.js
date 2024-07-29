8430ba4137e95687f9e62f20c9b8ec583c3a00d6
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-07-28 20:04:55 -0500

    Update service.js

--- btc-payroll/src/service.js
+++ btc-payroll/src/service.js
@@ -3,8 +3,11 @@ import axios from 'axios';
 
 const gustoAPI = axios.create({
   baseURL: 'https://api.gusto-demo.com/v1/companies/ea19d4e7-0a12-4bf9-9e5b-65242fa638da/employees',
+  withCredentials: false,
   headers: {
     Authorization: `Bearer ${process.env.REACT_APP_GUSTO_API_KEY}`,
+    'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
   },
 });
 
