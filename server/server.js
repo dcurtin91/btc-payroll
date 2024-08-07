cd70e7171810f62c407be507044124c6616bc48a
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-07 13:51:10 -0500

    server updates

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -32,6 +33,7 @@ const getEmployees = async () => {
   }
 };
 
+getEmployees();
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
