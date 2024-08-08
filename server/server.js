d4b7b8d6ac05a1f059a6e1e44c75af50e5911aff
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-07 20:57:02 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,15 +10,12 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const gustoAPI = axios.create({
   baseURL: 'https://api.gusto-demo.com/v1/companies/ea19d4e7-0a12-4bf9-9e5b-65242fa638da/employees',
   withCredentials: false,
   headers: {
     Authorization: `Bearer ${process.env.REACT_APP_GUSTO_API_KEY}`,
-    'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json',
+    'Content-Type': 'application/json',
   },
 });
 
@@ -26,14 +23,21 @@ const getEmployees = async () => {
   try {
     const response = await gustoAPI.get('/');
     return response.data;
-    
   } catch (error) {
     console.error('Error fetching employees:', error);
     throw error;
   }
 };
 
-getEmployees();
+// Define a route to get employees
+app.get('/employees', async (req, res) => {
+  try {
+    const employees = await getEmployees();
+    res.json(employees);
+  } catch (error) {
+    res.status(500).json({ message: 'Error fetching employees' });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
