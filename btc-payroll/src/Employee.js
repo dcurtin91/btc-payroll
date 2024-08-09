943afeb0eeb2274ef0176af02f612822b30680bd
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-09 11:53:04 -0500

    Update Employee.js

--- btc-payroll/src/Employee.js
+++ btc-payroll/src/Employee.js
@@ -10,6 +10,7 @@ const EmployeeList = () => {
     const fetchEmployees = async () => {
       try {
         const response = await axios.get('http://localhost:3000/employees');
+        console.log('Fetched employees:', response.data);
         setEmployees(response.data);
       } catch (err) {
         setError('Error fetching employees');
@@ -29,8 +30,8 @@ const EmployeeList = () => {
       <h2>Employee List</h2>
       <ul>
         {employees.map((employee) => (
-          <li key={employee.id}>
-            <strong>Name:</strong> {employee.name}<br />
+          <li key={employee.uuid}>
+            <strong>Name:</strong> {`${employee.first_name} ${employee.middle_initial || ''} ${employee.last_name}`}<br />
             <strong>Email:</strong> {employee.email}
           </li>
         ))}
