71498979d72b8f28268dab0ab260797bb0b0fd61
dcurtin91 <109159085+dcurtin91@users.noreply.github.com>
2024-08-12 11:19:54 -0500

    auth

--- btc-payroll/src/App.js
+++ btc-payroll/src/App.js
@@ -2,12 +2,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import EmployeeList from './Employee';
+import Login from './Login';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<EmployeeList />}exact />
+        <Route path="/login" element={<Login />}exact />
       </Routes>
     </Router>
   );
--- btc-payroll/src/Login.jsx
+++ btc-payroll/src/Login.jsx
@@ -1,7 +1,31 @@
-function Login() {
+import { Auth0Provider, useAuth0 } from '@auth0/auth0-react';
+
+const Login = () => {
     return (
-        <button>Login</button>
+        <Auth0Provider
+        domain=''
+        clientId=''
+        redirectUri='david-curtin.com'>
+            <Authenticated />
+        </Auth0Provider>
     )
 }
+
+const Authenticated = () => {
+    const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0(); 
+
+    return (
+        <div>
+            {isAuthenticated ? 
+            (
+                <div>{user.name}
+                <button onClick={() => logout()}>Logout</button>
+                </div>
+            ) :  (
+                <button onClick={() => loginWithRedirect()}>Login</button>
+            )}
+        </div>
+    )
+};
 
 export default Login;
